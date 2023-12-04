8dee34c312dbd580dc96e140dc124b47746c2f48
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-04 17:33:28 +0000

    Added user homepage view and template

--- Profile/views.py
+++ Profile/views.py
@@ -1,3 +1,22 @@
+from django.views import View
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from .utils import GetUserProfile
 
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
+class HomeView(View):
+    template_name = 'profile/home.html'
+
+    def get(self, request):
+        user_profile = GetUserProfile(request.user)
+        context = {
+            'user_profile': user_profile.get_profile(), 
+            'group_members_count': user_profile.group_members_count(),
+            'santa_greet': user_profile.getSantaGreet(),
+            }
+        return render(request, self.template_name, context)
+        
+    def post(self, request):
+        pass
