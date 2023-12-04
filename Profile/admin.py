48c30975125f15b1dedb007c2f275f7b7e48123c
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-04 00:03:22 +0000

    User profile and santa group
    Added user profile and santa group model with admin registeration

--- Profile/admin.py
+++ Profile/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import SantaGroup, UserProfile
 
 # Register your models here.
+class SantaGroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'date_created', 'date_updated')
+    list_filter = ('date_created', 'date_updated')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_pic', 'group_id')
+    list_filter = ('group_id',)
+
+admin.site.register(SantaGroup, SantaGroupAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
--- Profile/models.py
+++ Profile/models.py
@@ -1,3 +1,22 @@
+import uuid
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class SantaGroup(models.Model):
+    group_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    group_name = models.CharField(max_length=50, unique=True)
+    date_created = models.DateField(default=date.today)
+    date_updated = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.group_name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.CharField(max_length=200, blank=True)    
+    group_id = models.ForeignKey(SantaGroup, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
