1fdfbc92353592ec8e4fa4515a8dc63b97bb7ee4
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-04 23:21:21 +0000

    Added get members list function to profile utils

--- Profile/utils.py
+++ Profile/utils.py
@@ -1,4 +1,4 @@
-from .models import UserProfile
+from .models import UserProfile, Pick, SantaGroup
 from django.core.exceptions import ObjectDoesNotExist
 
 class GetUserProfile():
@@ -11,16 +11,32 @@ class GetUserProfile():
         except UserProfile.ObjectDoesNotExist:
             return None
 
-    def group_members_count(self):
-        try:
-            return UserProfile.objects.filter(group_id=self.get_profile().group_id).count()
-        except UserProfile.ObjectDoesNotExist:
-            return None
-
-    def getSantaGreet(self):
+    def get_santa_greet(self):
         if self.get_profile().gender == "Male":
             return 'a good boy'
         elif self.get_profile().gender == "Female":
             return 'a good girl'
         else:
             return 'good'
+
+    def get_wrapped(self):
+        try:
+            return UserProfile.objects.get(user=self.user).is_wrapped
+        except UserProfile.ObjectDoesNotExist:
+            return None
+
+    def group_members_count(self):
+        try:
+            return UserProfile.objects.filter(group_id=self.get_profile().group_id).count()
+        except UserProfile.ObjectDoesNotExist:
+            return None
+
+    def get_group_members_list(self):
+        try:
+            group_list = list(UserProfile.objects.filter(group_id=self.get_profile().group_id).exclude(user=self.user).values_list('full_name', flat=True))
+            for member in group_list:
+                if Pick.objects.filter(group_id=self.get_profile().group_id, full_name=member).exists():
+                    group_list.remove(member)
+            return group_list
+        except UserProfile.ObjectDoesNotExist:
+            return None
