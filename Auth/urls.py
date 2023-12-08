b18571fb4fba62ff09bd987f4460ec72fa9edf9e
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-08 13:10:48 +0000

    Custom login view, template and url

--- Auth/urls.py
+++ Auth/urls.py
@@ -1,10 +1,10 @@
 from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .forms import codeForm
+from .forms import loginForm, codeForm
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name = "auth/login.html"), name='login'),
+    path('login/', views.LoginView.as_view(form=loginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('2fa/', views.ConfirmView.as_view(form=codeForm), name='2fa'),
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -4,13 +4,53 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from Profile.models import UserProfile
+from django.contrib.auth.models import User
 from .utils import authCodeGenerator, sendEmail
 from django.contrib.auth import authenticate, login
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.hashers import check_password
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+class LoginView(View):
+    template_name = 'auth/login.html'
+    form = None
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            context = {'form': self.form}
+            return render(request, self.template_name, context)
+
+    def post(self, request):
+        post_form = self.form(request.POST)
+        if post_form.is_valid():
+            username = post_form.cleaned_data.get('username')
+            password = post_form.cleaned_data.get('password')
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return render(request, self.template_name, {'form': post_form, 'userError': True})
+            else:
+                if not check_password(password, user.password):
+                    return render(request, self.template_name, {'form': post_form, 'passwordError': True})
+                else:
+                    user = authenticate(request, username=username, password=password)
+                    if user is not None:
+                        login(request, user)
+                        user_profile = UserProfile.objects.get(user=user)
+                        if user_profile.auth_code == None:
+                            user_profile.auth_code = authCodeGenerator()
+                            user_profile.save()
+                        sending_email = sendEmail(request, user, user_profile.auth_code)
+                        return redirect('2fa')
+        else:
+            return render(request, self.template_name, {'form': post_form, 'formError': True})
+        
+
+
 @method_decorator(login_required, name='dispatch')
 @method_decorator(csrf_protect, name='dispatch')
 class ConfirmView(View):
