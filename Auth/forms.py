e5066dfef51198d76347c977ccf71a8296a9dab3
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-08 13:10:21 +0000

    Added custom login form

--- Auth/forms.py
+++ Auth/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
+class loginForm(forms.Form):
+    username = forms.EmailField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'username', 'name': 'username', 'placeholder': 'Enter username'}))
+    password = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password', 'name': 'password', 'placeholder': 'Enter password'}))
+
 class codeForm(forms.Form):
     code = forms.CharField(max_length=6, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'code', 'name': 'code', 'placeholder': 'Enter code'}) )
