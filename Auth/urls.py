c628ec181cc190d2bd2301f6fd8e139677866980
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-09 14:30:10 +0000

    Added register url and view

--- Auth/urls.py
+++ Auth/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .forms import loginForm, codeForm
+from .forms import loginForm, registerForm, codeForm
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(form=loginForm), name='login'),
+    path('register/', views.RegisterView.as_view(form=registerForm), name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('2fa/', views.ConfirmView.as_view(form=codeForm), name='2fa'),
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -48,7 +48,42 @@ class LoginView(View):
                         return redirect('2fa')
         else:
             return render(request, self.template_name, {'form': post_form, 'formError': True})
-        
+
+@method_decorator(csrf_protect, name='dispatch')
+class RegisterView(View):
+    template_name = 'auth/register.html'
+    form = None
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            context = {'form': self.form}
+            return render(request, self.template_name, context)
+
+    def post(self, request):
+        post_form = self.form(request.POST)
+        if post_form.is_valid():
+            username = post_form.cleaned_data.get('email')
+            password = post_form.cleaned_data.get('password')
+            email = post_form.cleaned_data.get('email')
+            first_name = post_form.cleaned_data.get('first_name')
+            last_name = post_form.cleaned_data.get('last_name')
+            gender = post_form.cleaned_data.get('gender')
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                user_profile = UserProfile.objects.create(user=user, gender=gender)
+                user_profile.save()
+                login(request, user)
+                return redirect('home')
+            else:
+                context = {'form': post_form, 'registerError': True}
+                return render(request, self.template_name, context)
+        else:
+            return render(request, self.template_name, {'form': post_form, 'formError': True})
 
 
 @method_decorator(login_required, name='dispatch')
