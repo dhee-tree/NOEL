e1bca00ee15ddb9772fc9ec48bb8fc69d66fbe90
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-10 14:10:07 +0000

    Added group admin classes

--- Group/admin.py
+++ Group/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import SantaGroup, Pick, GroupMember
 
 # Register your models here.
+class SantaGroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'group_code', 'date_created', 'date_updated')
+    list_filter = ('date_created', 'date_updated')
+
+
+class PickAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'picked_by', 'group_id', 'date_picked')
+    list_filter = ('group_id', 'date_picked')
+
+
+class GroupMemberAdmin(admin.ModelAdmin):
+    list_display = ('group_id', 'user_id', 'date_created', 'date_updated')
+    list_filter = ('date_created', 'date_updated')
+
+
+admin.site.register(SantaGroup, SantaGroupAdmin)
+admin.site.register(Pick, PickAdmin)
+admin.site.register(GroupMember, GroupMemberAdmin)
