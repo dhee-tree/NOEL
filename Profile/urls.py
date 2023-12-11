fad2b940698fc1bc4da7167eecbdd6cfef5785c6
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-11 22:46:49 +0000

    Removed wrapped and unwrapped views from profile

--- Profile/urls.py
+++ Profile/urls.py
@@ -4,6 +4,4 @@ from django.urls import path
 
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
-    path('wrapped/', views.WrappedView.as_view(), name='wrapped'),
-    path('unwrapped/', views.UnwrappedView.as_view(), name='unwrapped'),
 ]
