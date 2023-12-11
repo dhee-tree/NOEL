5a173632688cd27fd87a2fa67adde37a94f0cba1
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-11 14:55:52 +0000

    Added group create and view url

--- Group/urls.py
+++ Group/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name='group_home'),
-    path('create/', views.CreateView.as_view(), name='group_create'),
+    path('create/', views.CreateGroupView.as_view(), name='group_create'),
+    path('view/<str:group_name>/', views.ViewGroupView.as_view(), name='group_view'),
 ]
