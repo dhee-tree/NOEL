9a7c9f2f767e3e9a10e7bf9ae5043054b6ff8b73
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-12 03:37:25 +0000

    Added group delete url

--- Group/urls.py
+++ Group/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('view/<str:group_name>/wrapped/', views.WrappedView.as_view(), name='group_wrapped'),
     path('view/<str:group_name>/unwrapped/', views.UnwrappedView.as_view(), name='group_unwrapped'),
     # path('view/<str:group_name>/edit/', views.EditGroupView.as_view(), name='group_edit'),
-    # path('view/<str:group_name>/delete/', views.DeleteGroupView.as_view(), name='group_delete'),
+    path('delete/<str:group_name>', views.DeleteGroupView.as_view(), name='group_delete'),
     # path('view/<str:group_name>/add/', views.AddMemberView.as_view(), name='group_add_member'),
     # path('view/<str:group_name>/remove/', views.RemoveMemberView.as_view(), name='group_remove_member'),
 ]
