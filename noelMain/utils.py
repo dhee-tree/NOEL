88652e209ec43ba8d392502eded1c038ccc8ee43
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-14 14:00:03 +0000

    Moved send mail from auth app to main app

---
+++ noelMain/utils.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+
+def sendEmail(request, user, code):
+    subject = 'Pssst! Your secret code is...'
+    html_message = render_to_string('auth/2fa_email.html', {'user': user, 'code': code})
+    plain_message = strip_tags(html_message)
+    from_email = settings.EMAIL_HOST_USER
+    to = user.email
+    send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+    return HttpResponse('Email sent')
