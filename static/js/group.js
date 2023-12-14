30395b97c57ef9cac7ec359c126ca1f16ab7283c
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-12-14 17:08:56 +0000

    Modified whatsapp invite message

--- static/js/group.js
+++ static/js/group.js
@@ -7,3 +7,19 @@ const copyToClipboard = async () => {
     console.error("Failed to copy to clipboard:", error);
   }
 };
+
+// Share invite code
+function encodeMessage(message) {
+    return encodeURIComponent(message);
+}
+
+function createWhatsAppLink(message) {
+    const encodedMessage = encodeMessage(message);
+    return `https://api.whatsapp.com/send?text=${encodedMessage}`;
+}
+
+const shareMessage = "Hello, join me on NOEL. My group name is {{group}}. Use this code {{group.group_code}} to join my group. Register with https://noel.ighomena.me/auth/register/";
+
+const whatsappLink = createWhatsAppLink(shareMessage);
+
+document.getElementById('whatsappLink').setAttribute('href', whatsappLink);
