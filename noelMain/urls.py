196752a9c6169c551b16e9b2bb65e2a6599a5163
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-12-16 21:47:27 +0000

    Added contact form to contact url

--- noelMain/urls.py
+++ noelMain/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from .forms import ContactForm
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('contact/', views.ContactView.as_view(form=ContactForm), name='contact'),
     path('privacy/', views.PrivacyView.as_view(), name='privacy'),
     path('terms/', views.TermsView.as_view(), name='terms'),
     path('faq/', views.FAQView.as_view(), name='faq'),
