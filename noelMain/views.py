fd797953963cb399ec3d65b5cc8f38e5865b1e10
Ighomena Daniel Odebala <ighoodes@gmail.com>
2023-12-16 21:48:11 +0000

    Added get and post methods to contact views

--- noelMain/views.py
+++ noelMain/views.py
@@ -1,6 +1,8 @@
+from django.views import View
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from Mail.utils import MailManager
 
 # Create your views here.
 
@@ -18,8 +20,29 @@ def index(request):
 class AboutView(TemplateView):
     template_name = 'about.html'
 
-class ContactView(TemplateView):
+class ContactView(View):
     template_name = 'contact.html'
+    form = None
+
+    def get(self, request):
+        context = {
+            'form': self.form
+        }
+        return render(request, self.template_name, context)
+    
+    def post(self, request):
+        post_form = self.form(request.POST)
+        if post_form.is_valid():
+            name = post_form.cleaned_data['name']
+            email = post_form.cleaned_data['email']
+            message = post_form.cleaned_data['message']
+            mail_manager = MailManager(email)
+            mail_manager.send_contact_message(name, message)
+            messages.success(request, 'Your message has been sent successfully.')
+            return redirect('contact')
+        else:
+            messages.error(request, 'Your message was not sent. Please try again.')
+            return redirect('contact')
 
 class PrivacyView(TemplateView):
     template_name = 'privacy.html'
