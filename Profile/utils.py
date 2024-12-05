13ad2b7559d9a15eae7572ee818fdbd8783d0df6
Ighomena Daniel Odebala <ighoodes@gmail.com>
2024-12-05 21:13:12 +0000

    Add random santa greeting

--- Profile/utils.py
+++ Profile/utils.py
@@ -1,6 +1,17 @@
 from .models import UserProfile
-from Group.models import Pick
 from django.core.exceptions import ObjectDoesNotExist
+import random
+
+GREETINGS = [
+    "Well done, you've been good! I have gift for you.",
+    "Ah, just the person I was looking for—there is a gift for you!",
+    "You've been so good this year! I have something special just for you.",
+    "Here you go, a little something for being so wonderful!",
+    "You've earned this—happy holidays and enjoy your gift!",
+    "I have a special gift for you, just for being so good!",
+    "You've been good this year, so here's a gift for you!",
+    "You've been so good, I have a gift for you!",
+]
 
 class GetUserProfile():
     def __init__(self, user):
@@ -15,12 +26,7 @@ class GetUserProfile():
 
     def get_santa_greet(self):
         """Returns appropiate greeting based on the user gender"""
-        if self.get_profile().gender == "Male":
-            return 'a good boy'
-        elif self.get_profile().gender == "Female":
-            return 'a good girl'
-        else:
-            return 'good'
+        return f"Ho Ho Ho! {self.user.first_name}, {random.choice(GREETINGS)}"
 
     def update_profile(self, gender, address):
         """Updates the user profile of the loggedin user"""
