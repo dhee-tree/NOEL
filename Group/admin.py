dee0fa3a4fb36cc27b762855602487ccd9f1cc05
Ighomena Daniel Odebala <ighoodes@gmail.com>
2024-12-06 03:03:09 +0000

    Add is archived filter

--- Group/admin.py
+++ Group/admin.py
@@ -4,17 +4,17 @@ from .models import SantaGroup, Pick, GroupMember
 # Register your models here.
 class SantaGroupAdmin(admin.ModelAdmin):
     list_display = ('group_name', 'group_code', 'is_open', 'created_by', 'date_created', 'date_updated')
-    list_filter = ('is_open', 'created_by', 'date_created', 'date_updated')
+    list_filter = ('is_open', 'created_by', 'date_created', 'date_updated', 'is_archived')
 
 
 class PickAdmin(admin.ModelAdmin):
     list_display = ('picked_by', 'group_id', 'date_picked')
-    list_filter = ('group_id', 'date_picked')
+    list_filter = ('group_id', 'date_picked', 'is_archived')
 
 
 class GroupMemberAdmin(admin.ModelAdmin):
     list_display = ('group_id', 'user_profile_id', 'is_wrapped', 'date_created', 'date_updated')
-    list_filter = ('group_id', 'is_wrapped')
+    list_filter = ('group_id', 'is_wrapped', 'is_archived')
 
 
 admin.site.register(SantaGroup, SantaGroupAdmin)
