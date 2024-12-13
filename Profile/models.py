868493d6fcea4658b45cedd04e40d74667f305c7
Ighomena Daniel Odebala <ighoodes@gmail.com>
2024-12-13 23:25:26 +0000

    Increase wish item link characters

--- Profile/models.py
+++ Profile/models.py
@@ -33,7 +33,7 @@ class WishListItem(models.Model):
     item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=False)
-    link = models.CharField(max_length=255, null=True, blank=True)
+    link = models.CharField(max_length=1000, null=True, blank=True)
     date_created = models.DateField(default=date.today)
     date_updated = models.DateField(default=date.today)
 
