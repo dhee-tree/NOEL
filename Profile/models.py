72e77d25386b873a26e87ee5f4e6a6c32d003b1e
Ighomena Daniel Odebala <ighoodes@gmail.com>
2024-12-13 23:06:35 +0000

    Increase name length

--- Profile/models.py
+++ Profile/models.py
@@ -32,7 +32,7 @@ class UserProfile(models.Model):
 class WishListItem(models.Model):
     item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=255, blank=False)
     link = models.CharField(max_length=255, null=True, blank=True)
     date_created = models.DateField(default=date.today)
     date_updated = models.DateField(default=date.today)
