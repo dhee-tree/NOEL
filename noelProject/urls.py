27a7a7ecb7cf4fffb7ed68185e17e4129aa8e571
Ighomena Daniel Odebala <ighoodes@gmail.com>
2025-07-17 22:48:52 +0100

    Add api app

--- noelProject/urls.py
+++ noelProject/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', include('noelMain.urls')),
@@ -24,4 +29,10 @@ urlpatterns = [
     path('group/', include('Group.urls')),
     path('mail/', include('Mail.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
