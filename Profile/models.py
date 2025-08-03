92071246113bb0e4ee45cfe00b643e206bbe838d
Ighomena Daniel Odebala <ighoodes@gmail.com>
2025-08-03 01:06:45 +0100

    Add role to user profile

--- Profile/models.py
+++ Profile/models.py
@@ -4,19 +4,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class UserProfile(models.Model):
     gender_choices = (
         ('Male', 'Male'),
         ('Female', 'Female'),
         ('Prefer not to say', 'Prefer not to say'),
     )
+
+    class Role(models.TextChoices):
+        USER = 'USER', 'User'
+        ADMIN = 'ADMIN', 'Admin'
+        SUPPORT = 'SUPPORT', 'Support'
+        MAINTAINER = 'MAINTAINER', 'Maintainer'
+
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-    google_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    google_id = models.CharField(
+        max_length=255, unique=True, null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(
+        max_length=50,
+        choices=Role.choices,
+        default=Role.USER
+    )
     full_name = models.CharField(max_length=50, blank=True)
-    gender = models.CharField(max_length=50, choices=gender_choices, blank=True)
+    gender = models.CharField(
+        max_length=50, choices=gender_choices, blank=True)
     address = models.CharField(max_length=200, blank=True)
-    profile_pic = models.CharField(max_length=200, blank=True)    
+    profile_pic = models.CharField(max_length=200, blank=True)
     is_verified = models.BooleanField(default=False)
     verification_code = models.CharField(max_length=8, blank=True, default='')
     date_created = models.DateField(default=date.today)
@@ -28,10 +43,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.full_name
-    
+
 
 class WishListItem(models.Model):
-    item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    item_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=False)
     link = models.CharField(max_length=1000, null=True, blank=True)
